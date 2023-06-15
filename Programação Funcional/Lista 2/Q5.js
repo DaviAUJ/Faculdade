438550d23cc1af79b60db8fbf5993fbafad4d3fd
plantsX <84574289+DaviAUJ@users.noreply.github.com>
2023-06-15 20:07:11 -0300

    Update Q5.js

--- Programação Funcional/Lista 2/Q5.js
+++ Programação Funcional/Lista 2/Q5.js
@@ -1,20 +1,20 @@
 function calcularDistancia(ponto1, ponto2) {
-    if(ponto1.X === ponto1.Y) {
-        return Math.abs(ponto1.Y - ponto1.X)
-    } else if(ponto2.X === ponto2.Y) {
-        return Math.abs(ponto2.Y - ponto2.X)
+    if(ponto1.X === ponto2.X) {
+        return Math.abs(ponto1.Y - ponto2.Y)
+    } else if(ponto1.Y === ponto2.Y) {
+        return Math.abs(ponto1.X - ponto2.X)
     } else {
         return ((ponto2.X - ponto1.X) ** 2 + (ponto2.Y - ponto1.Y) ** 2 ) ** (1/2)
     }
 }
 
 const A = {
-    X: -24,
-    Y: -27
+    X: 1,
+    Y: 5
 }
 const B = {
-    X: -3,
-    Y: 1
+    X: -10,
+    Y: 5
 }
 
 console.log(`A distância entre os dois ponto é ${calcularDistancia(A, B)}`)

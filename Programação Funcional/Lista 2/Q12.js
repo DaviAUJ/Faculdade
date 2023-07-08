8b2de6af631cf2413491d4c003b741c7937dd530
plantsX <84574289+DaviAUJ@users.noreply.github.com>
2023-07-08 18:19:11 -0300

    mudanças

--- Programação Funcional/Lista 2/Q12.js
+++ Programação Funcional/Lista 2/Q12.js
@@ -1,10 +1,18 @@
-function quadFunction(a, b, c) {
-    const x = [
-        (-b + (b ** 2 - 4 * a * c) ** (1/2)) / (2 * a),
-        (-b - (b ** 2 - 4 * a * c) ** (1/2)) / (2 * a)
-    ]
+const quadFunction = (a, b, c) => {
+    const delta = b ** 2 - 4 * a * c
     
-    return x
+    if(delta > 0) {
+        return [
+            (-b + delta ** (1/2)) / (2 * a),
+            (-b - delta ** (1/2)) / (2 * a)
+        ]
+
+    } else if(delta === 0) {
+        return -b / (2 * a)
+
+    } else {
+        return "As raizes não são reais"
+    }
 }
 
-console.log(quadFunction(1, 6, -7))
+console.log(quadFunction(-1, 20, 0)) 

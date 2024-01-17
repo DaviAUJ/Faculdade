ae0ebd956753a65bc889d4e6c19dde39883ff72c
Davi Araújo <84574289+DaviAUJ@users.noreply.github.com>
2024-01-17 13:49:48 -0300

    Update E02.c

--- Programação Imperativa/Lista 02/E02.c
+++ Programação Imperativa/Lista 02/E02.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+// No pré-escolar “Balão Mágico” o número máximo de alunos de uma
+// turma é definido anualmente, após as reformas de fim de ano e
+// evolução curricular. Escrever programa em C onde se lê o número
+// total de alunos matriculados, o número máximo de alunos por
+// turma e exibir o número de turmas a serem formadas.
+
 int aluno(int totalAlunos, int maxAlunos) {
     if(totalAlunos % maxAlunos == 0) { return totalAlunos / maxAlunos; }
     else { return totalAlunos / maxAlunos - (totalAlunos / maxAlunos) % 1 + 1; }

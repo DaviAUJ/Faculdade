19e627424e7f95176123baf14c4caea8acc603db
Davi Araújo <84574289+DaviAUJ@users.noreply.github.com>
2024-01-17 14:13:01 -0300

    Update E04.c

--- Programação Imperativa/Lista 02/E04.c
+++ Programação Imperativa/Lista 02/E04.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// Escrever um programa em C para ler uma temperatura em Celsius C e
+// a retornar em Fahrenheit e em Kelvin. Fórmulas de conversão para
+// Fahrenheit: F = (C * 9/5) + 32; para Kelvin: K = C + 273,5.
+// Criar uma função para cada conversão.
+
 double CParaF(double celsius) { return celsius * 9 / 5 + 32; }
 double CParaK(double celsius) { return celsius + 273.5; }
 

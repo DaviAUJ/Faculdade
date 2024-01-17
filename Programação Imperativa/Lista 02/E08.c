d52c56cf389fb7866937df456b6858e78d01811b
Davi Araújo <84574289+DaviAUJ@users.noreply.github.com>
2024-01-17 14:35:24 -0300

    Update E08.c

--- Programação Imperativa/Lista 02/E08.c
+++ Programação Imperativa/Lista 02/E08.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
-// Cotação do dólar e euro do dia 16/12/2023
-// 1 real == 0.20 Dolár == 0.19 euro
+// Escrever um programa em C para ler um valor V em reais, taxa de
+// câmbio fixa de dólar e taxa de câmbio fixa de euro e exibir em tela os
+// correspondentes a V em dólar e em euro. Para tanto, criar função F em que
+// dados valor e taxa de câmbio, retorna o valor convertido. Criar módulo com
+// aplicação de F, parâmetros de entrada e dois parâmetros de saída: valor em
+// dólar e valor em euro.
 
-const double DOLAR = 0.2;
-const double EURO = 0.19;
-
-double cambio(double vReal, double* saidaDolar, double* saidaEuro) {
-    *saidaDolar = vReal * DOLAR;
-    *saidaEuro = vReal * EURO;
+double cambio(double vReal, double cDolar, double cEuro, double* saidaDolar, double* saidaEuro) {
+    *saidaDolar = vReal * cDolar;
+    *saidaEuro = vReal * cEuro;
 }
 
 int main() {
-    double real, dolar, euro;
+    double real, cambioDolar, cambioEuro, dolar, euro;
 
     printf("Digite um valor em reais: ");
     scanf("%lf", &real);
 
-    cambio(real, &dolar, &euro);
+    printf("Digite o valor de 1 real em dólar: ");
+    scanf("%lf", &cambioDolar);
 
-    printf("\nIsto em dolar e: %.2lf", dolar);
-    printf("\nIsto em euro e: %.2lf", euro);
+    printf("Digite o valor de 1 real em euro: ");
+    scanf("%lf", &cambioEuro);
+    
+    cambio(real, cambioDolar, cambioEuro, &dolar, &euro);
+
+    printf("\n%.2lf em dolar e: %.2lf", real, dolar);
+    printf("\n%.2lf em euro e: %.2lf", real, euro);
 
     return 0;
 }

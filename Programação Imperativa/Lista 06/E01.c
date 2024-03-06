aaf235faafaed39ede901c5d661dd40118ae52b8
plantsX <84574289+DaviAUJ@users.noreply.github.com>
2024-03-06 02:28:13 -0300

    atividade

---
+++ Programação Imperativa/Lista 06/E01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    // Como mostrado no slide, este char permitiria apenas 20 caracteres porem eu consigo associar mais quando usado o scanf ou o gets
+    // para testar use a palavra desinstitucionalizado, ela tem 21 letras, portanto não deveria ser possível usa-lá como input
+    char palavraMin[21], palavraMai[21] = "";
+
+    printf("Digite uma palavra: ");
+    scanf("%s", palavraMin);
+
+    // Transformação em maiuscula
+    for(int cont = 0; cont < strlen(palavraMin); cont++) {
+        palavraMai[cont] = toupper(palavraMin[cont]); // Transformado char por char em maiúscula
+    }
+
+    printf("A palavra %s toda maiuscula e: %s", palavraMin, palavraMai);
+}

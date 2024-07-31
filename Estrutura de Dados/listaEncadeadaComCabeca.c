0b6f8efa928677fb67d5c4bd79fbae340f4971ef
Davi Araújo <84574289+DaviAUJ@users.noreply.github.com>
2024-07-31 03:01:26 -0300

    atividade

--- Estrutura de Dados/listaEncadeadaComCabeca.c
+++ Estrutura de Dados/listaEncadeadaComCabeca.c
@@ -12,11 +12,13 @@ typedef struct noCabeca {
     int quantidade;
 } noCabeca;
 
+
 void inicializa(noCabeca *cabeca) {
     cabeca->inicio = NULL;
     cabeca->fim = NULL;
     cabeca->quantidade = 0;
 }
+
 
 int criarNoInicial(noCabeca *cabeca, int valor) {
     no *noInicial = (no*) malloc(sizeof(no));
@@ -32,7 +34,8 @@ int criarNoInicial(noCabeca *cabeca, int valor) {
     return 1;
 }
 
-int adicionarNaFrente(noCabeca *cabeca, int valor) {
+
+int adicionarNoInicio(noCabeca *cabeca, int valor) {
     no *novoNo;
 
     if(cabeca->inicio == NULL) { criarNoInicial(cabeca, valor); }
@@ -50,6 +53,55 @@ int adicionarNaFrente(noCabeca *cabeca, int valor) {
     }
 }
 
+
+int adicionarNoFim(noCabeca *cabeca, int valor) {
+    no *novoNo;
+
+    if(cabeca->inicio == NULL) { criarNoInicial(cabeca, valor); }
+    else {
+        novoNo = (no*) malloc(sizeof(no));
+
+        if(novoNo == NULL) { return 0; }
+
+        novoNo->dado = valor;
+        novoNo->ponteiro = NULL;
+        cabeca->fim->ponteiro = novoNo; // Altera o ponteiro do antigo fim da lista
+        cabeca->fim = novoNo;
+        cabeca->quantidade++;
+
+        return 1;
+    }
+}
+
+
+int destruirLista(noCabeca *cabeca) {
+    no *noAtual, *noSeguinte;
+
+    // Caso onde não existe nenhum elemento na lista
+    if(cabeca->inicio == NULL) { return 0; }
+    
+    // Caso onde lista tem apenas um nó
+    if(cabeca->inicio->ponteiro == NULL) { free(cabeca->inicio); }
+    else {
+        // Caso onde lista tem dois ou mais nós
+        noAtual = cabeca->inicio;
+        noSeguinte = cabeca->inicio->ponteiro;
+    
+        while(noSeguinte != NULL) {
+            free(noAtual);
+            noAtual = noSeguinte;
+            noSeguinte = noSeguinte->ponteiro;
+        }
+
+        free(noAtual); // Libera mais uma vez pois o while deixa faltando um espaço de memória sobrando
+    }
+
+    // nó cabeça é reinicializado pois agora ele não está atrelado a nenhuma lista encadeada
+    inicializa(cabeca);
+    return 1;
+}
+
+
 int exibeLista(noCabeca *cabeca) {
     no *noAtual;
 
@@ -61,15 +113,40 @@ int exibeLista(noCabeca *cabeca) {
         printf("%d", noAtual->dado);
         noAtual = noAtual->ponteiro;
 
-        if(noAtual == NULL) { break; }
+        if(noAtual == NULL) { break; } 
 
         printf(", "); // Esse esquema aqui é só para aparecer bonitinho
     }
 
     printf(" ]");
+    printf("\nNumero de elementos: %d", cabeca->quantidade);
 
     return 1;
 }
+
+
+int procurarValor(noCabeca *cabeca, int valor) {
+    no *noAtual;
+    int index = 0, sair = 0;
+
+    if(cabeca->inicio == NULL) { return -1; }
+    noAtual = cabeca->inicio;
+
+    while(!sair) {
+        if(noAtual == NULL) { 
+            sair = 1; 
+            index = -1; 
+        } 
+        else if(noAtual->dado == valor) { sair = 1; } 
+        else {
+            noAtual = noAtual->ponteiro;
+            index++;
+        } 
+    }
+
+    return index;
+}
+
 
 int main() {
     noCabeca cabeca;
@@ -86,7 +163,7 @@ int main() {
     while(resp) {
         printf("Digite outro valor: ");
         scanf("%d", &entrada);
-        adicionarNaFrente(&cabeca, entrada);
+        adicionarNoFim(&cabeca, entrada);
 
         printf("Adicionar outro elemento(S/N)? ");
         scanf(" %c", &entrada);
@@ -96,7 +173,9 @@ int main() {
     }
 
     resp = exibeLista(&cabeca);
-    if(resp == 0) { printf("Lista vazia"); }
+    if(!resp) { printf("Lista vazia"); }
+
+    printf("\n%d", NULL == 20);
 
     return 0;
 }

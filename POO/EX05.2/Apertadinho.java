607bd49bd21b269937daa00fbf2525d394ad9d8f
Davi Araújo <84574289+DaviAUJ@users.noreply.github.com>
2024-08-08 00:12:34 -0300

    Atividade

--- POO/EX05.2/Apertadinho.java
+++ POO/EX05.2/Apertadinho.java
@@ -4,7 +4,7 @@ public class Apertadinho {
         return (int) output;
     }
 
-    private int NUMERO_SECRETO;
+    private int numSecreto;
     private int minimo;
     private int maximo;
     public int minTemp;
@@ -16,14 +16,45 @@ public class Apertadinho {
     }
 
     public void iniciar() {
-        NUMERO_SECRETO = randint(minimo, maximo);
+        numSecreto = randint(minimo, maximo);
         minTemp = minimo;
         maxTemp = maximo;
     }
 
-    public boolean receberChute(int chute) { return chute == NUMERO_SECRETO; }
+    public void mostrarIntervaloTemp() {
+        int minDiff = minTemp - minimo;
+        int divisor = (maximo - minimo) / 40;
+        int i = String.valueOf(minTemp).length() - 1;
+
+        while(i < minDiff / divisor) { 
+            System.out.printf(" "); 
+            i++;
+        }
+        System.out.printf("%d | ", minTemp);
+
+        while(i < maxTemp / divisor) { 
+            System.out.printf("-"); 
+            i++;
+        }
+        System.out.printf(" | %d", maxTemp);
+    }
+
+    public int receberChute(int chute) { 
+        if(chute > maxTemp || chute < minTemp) { return 2; }
+        if(chute == numSecreto) { return 1; }
+        else { return 0; } 
+    }
+
+    public void mudarIntervaloTemp(int chute) {
+        if(chute < numSecreto) { minTemp = chute; }
+        else if(chute > numSecreto) { maxTemp = chute; }
+    }
 
     public void setMinimo(int novoValor) { minimo = novoValor; }
 
     public void setMaximo(int novoValor) { maximo = novoValor; }
+
+    public int getMinimo() { return minimo; }
+
+    public int getMaximo() { return maximo; }
 }
--- POO/EX05.2/Partida.java
+++ POO/EX05.2/Partida.java
@@ -4,51 +4,94 @@ public class Partida {
     private static void p_Jogar(Apertadinho jogo) {
         Scanner entrada = new Scanner(System.in);
         int chute;
-        boolean estaCorreto;
+        int estaCorreto;
 
         jogo.iniciar();
 
+        OUTER:
         while(true) {
-            System.out.printf("Digite seu chute: ");
+            System.out.println("\n");
+            jogo.mostrarIntervaloTemp();
+            System.out.printf("\n\nDigite seu chute: ");
             chute = entrada.nextInt();
-
-            
-            
             estaCorreto = jogo.receberChute(chute);
-            if(estaCorreto) { break; }
-            
-            System.out.println("Chute Errado");
+
+            switch(estaCorreto) {
+                case 1 -> { break OUTER; }
+                case 2 -> System.out.println("\nChute fora do intervalo; Tente novamente");
+                default -> {
+                    jogo.mudarIntervaloTemp(chute);
+                    System.out.println("\nChute Errado");
+                }
+            }
         }
 
-        System.out.println("Chute Correto");
+        System.out.println("\nParabens você acertou o numero secreto");
     }
 
     private static void p_Config(Apertadinho jogo) {
-        System.out.println("Valores atuais");
-        System.out.println("Minimo: " + jogo.inicio);
-        System.out.println("Maximo: " + jogo.fim);
+        Scanner entrada = new Scanner(System.in);
+        int escolha;
+
+        do {
+            System.out.println("\n\nValores atuais");
+            System.out.println("Minimo: " + jogo.getMinimo());
+            System.out.println("Maximo: " + jogo.getMaximo());
+
+            System.out.println("\n1 - Ajustar valores");
+            System.out.println("2 - Restaurar padrao");
+            System.out.println("3 - Sair");
+
+            System.out.printf("\nEscolha: ");
+            escolha = entrada.nextInt();
+
+            switch(escolha) {
+                case 1 -> {
+                    System.out.printf("\nValor Minimo: ");
+                    jogo.setMinimo(entrada.nextInt());
+                    System.out.printf("Valor Maximo: ");
+                    jogo.setMaximo(entrada.nextInt());
+
+                    System.out.println("\nValores alterados");
+                }
+                case 2 -> {
+                    jogo.setMinimo(0);
+                    jogo.setMaximo(100);
+
+                    System.out.println("\nValores retornados ao padrao");
+                }
+                case 3 -> { System.out.println("\nRetornando ao menu inicial"); }
+            } 
+
+        } while(escolha != 3);
     }
 
     private static void p_Sair() {
-        
+        System.out.println("\n\nAte a proxima");
+        System.out.println("Desligando...");
     }
     
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int escolha;
 
-        System.out.println("1 - Jogar");
-        System.out.println("2 - Configuracoes");
-        System.out.println("3 - Sair");
-        System.out.printf("\nEscolha: ");
-        escolha = entrada.nextInt();
-        
-        Apertadinho jogo = new Apertadinho(0, 10);
+        Apertadinho jogo = new Apertadinho(0, 100);
 
-        switch(escolha) {
-        case 1 -> p_Jogar(jogo); 
-        case 2 -> p_Config(jogo);
-        case 3 -> p_Sair();
-        }
+        do {
+            System.out.println("\n\n----- BEM-VINDO AO APERTADINHO -----");
+
+            System.out.println("\n1 - Jogar");
+            System.out.println("2 - Configuracoes");
+            System.out.println("3 - Sair");
+            System.out.printf("\nEscolha: ");
+            escolha = entrada.nextInt();
+
+            switch(escolha) {
+            case 1 -> p_Jogar(jogo); 
+            case 2 -> p_Config(jogo);
+            case 3 -> p_Sair();
+            }
+
+        } while(escolha != 3);
     }
 }

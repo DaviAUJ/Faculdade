65f95f9e0d89181fd6efed6560d198c863f3fd1a
Davi Araújo <84574289+DaviAUJ@users.noreply.github.com>
2025-02-27 23:26:27 -0300

    projeto

--- PAA/projeto_compressao/davinascimento_202300027801_compressao.c
+++ PAA/projeto_compressao/davinascimento_202300027801_compressao.c
@@ -2,58 +2,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE* entrada;
-FILE* saida;
+FILE* arqEntrada;
+FILE* arqSaida;
 
 typedef struct string {
-    char* string;
-    int16_t tamanho;
+    uint8_t* string;
+    uint16_t tamanho;
 } st_String;
 
 st_String criarString(int16_t tamanho) {
     st_String saida;
 
-    saida.string = (char*) malloc(sizeof(char) * (tamanho + 1));
+    saida.string = (uint8_t*) malloc(sizeof(uint8_t) * (tamanho + 1));
     saida.string[tamanho] = '\0';
     saida.tamanho = tamanho;
 
     return saida;
 }
 
-int16_t pegarProxTam() {
-    int16_t saida;
+uint16_t pegarProxTam() {
+    uint16_t saida;
 
-    fscanf(entrada, "%d ", &saida);
+    fscanf(arqEntrada, "%hu ", &saida);
 
     return saida;
 }
 
-char pegarProxHex() {
-    char saida;
+uint8_t pegarProxHex() {
+    uint8_t saida;
 
-    fscanf(entrada, "%X ", &saida);
+    fscanf(arqEntrada, "%X ", &saida);
 
     return saida;
 }
 
 st_String pegarLinha() {
     st_String saida;
-    int16_t tamanho = pegarProxTam();
+    uint16_t tamanho = pegarProxTam();
 
     saida = criarString(tamanho);
 
-    for(int16_t a = 0; a < tamanho; a++) {
+    for(uint16_t a = 0; a < tamanho; a++) {
         saida.string[a] = pegarProxHex();
     }
 
     return saida;
 }
 
+void printarLinha(st_String string) {
+    for(int c = 0; c < string.tamanho; c++) {
+        fprintf(arqSaida, "%02X", string.string[c]);
+    }
+}
+
+st_String RLF(st_String string) {
+    uint8_t* stringComprimida = (uint8_t*) malloc(sizeof(uint8_t) * (string.tamanho + 1));
+    stringComprimida[string.tamanho] = '\0';
+
+    uint16_t indexCompressao = 0;
+    uint8_t repeticoes = 1;
+    uint8_t referencia = string.string[0];
+
+    for(uint16_t b = 1; b < string.tamanho + 1; b++) {
+        if(string.string[b] == referencia && repeticoes < UINT8_MAX) {
+            repeticoes++;
+        }
+        else {
+            stringComprimida[indexCompressao++] = repeticoes;
+            stringComprimida[indexCompressao++] = referencia;
+
+            repeticoes = 1;
+            referencia = string.string[b];
+        }
+    }
+
+    st_String saida;
+    saida.tamanho = indexCompressao;
+    realloc(stringComprimida, sizeof(uint8_t) * (saida.tamanho + 1));
+    stringComprimida[saida.tamanho] = '\0';
+    saida.string = stringComprimida;
+
+    return saida;
+}
+
+st_String Huffman(st_String string) {
+
+}
+
 int main(int argc, char** argv) {
-    entrada = fopen(argv[1], "r");
-    saida = fopen(argv[2], "w");
+    arqEntrada = fopen(argv[1], "r");
+    arqSaida = fopen(argv[2], "w");
 
+    uint32_t linhas;
+    fscanf(arqEntrada, "%d", &linhas);
 
+    st_String teste = pegarLinha();
+    st_String testesaida = RLF(teste);
+
+    printarLinha(testesaida);
 
     return 0;
 }

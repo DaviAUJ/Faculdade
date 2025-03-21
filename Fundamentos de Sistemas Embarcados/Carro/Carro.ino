5295d938f9d09bdb8204721d60059f62e24cf30b
Davi Araújo <84574289+DaviAUJ@users.noreply.github.com>
2025-03-21 19:43:25 -0300

    Trabalho

---
+++ Fundamentos de Sistemas Embarcados/Carro/Carro.ino
@@ -0,0 +1,379 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <Math.h>
+
+// Constantes do display
+#define DISPLAYADDR 0x27
+#define DISPLAYCOL 20
+#define DISPLAYROW 4
+/* 
+
+  LAYOUT DO DISPLAY:
+
+  Velocidade: xxxx kmh
+  Proximidade: [xxxxx]
+  Farol:       xxxxxxx
+  A/C:         xxxxxxx
+
+*/
+
+// Pinos usados
+#define KEYPIN A0
+#define PEDALPIN A1
+#define SMALLLEDPIN 2
+#define HEADLIGHTBUTTONPIN 4
+#define BUZZERPIN 6
+#define BIGLEDPIN 7
+#define ACBUTTONPIN 8
+#define ECHOPIN 12
+#define TRIGPIN 13
+
+// Tempo entre cada ciclo do programa
+#define CYCLETIME 10
+
+// Demarcadores da posição da chave
+#define SYSTEM 300
+#define ENGINE 900
+
+// Instanciando display
+LiquidCrystal_I2C lcd(DISPLAYADDR, DISPLAYCOL, DISPLAYROW);
+
+// Variáveis globais
+bool ACon; // true = ligado, false = desligado
+uint8_t headlightState; // 0 = desligado, 1 = farol baixo, 2 = farol alto
+uint8_t carState; // 0 = desligado, 1 = painel ligado, 2 = painel e motor ligado
+int8_t speed; // Velocidade do carro
+int16_t keyValue; // Valor da chave de ignição
+uint32_t cycle; // Contador de ciclos
+float distance; // Distancia de algum objeto do sensor
+
+// Chamada quando o motor e painel estão desligados
+void startSystem() {
+  // Tenta ligar o painel
+  if(keyValue >= SYSTEM) {
+    // Ligando painel
+    lcd.backlight();
+    lcd.print("Velocidade: ");
+    lcd.setCursor(0, 1);
+    lcd.print("Proximidade: ");
+    lcd.setCursor(0, 2);
+    lcd.print("Farol:     Desligado");
+    lcd.setCursor(0, 3);
+    lcd.print("A/C: ");
+
+    // Permite acionar a buzina
+    analogWrite(BUZZERPIN, 200);
+
+    carState = 1;
+  }
+}
+
+// Função responsável por atualizar a parte da velocidade do display
+// Chamada apenas quando há alteração nos valores
+void updateDisplaySpeed() {
+  lcd.setCursor(12, 0);
+  
+  // Estes ifs aqui estão aí para formatar as informações no lugar certo
+  // dependendo da quantidade de dígitos no número
+  if(speed >= 100) {
+    lcd.print(" ");
+    lcd.print(speed);
+  }
+  else if(speed >= 10) {
+    lcd.print("  ");
+    lcd.print(speed);
+  }
+  else if(speed >= 0) {
+    lcd.print("   ");
+    lcd.print(speed);
+  }
+  else if(speed >= -9) {
+    lcd.print("  ");
+    lcd.print(speed);
+  }
+  else if(speed >= -99) {
+    lcd.print(" ");
+    lcd.print(speed);
+  }
+  else {
+    lcd.print(speed);
+  }
+}
+
+// Responsável por pegar o valor do potenciômetro 2 e transformar em aceleração
+// Além de alterar o valor da velocidade dentro do sistema
+// Função chamada a cada 10 ciclos
+void updateSpeed() {
+  // Armazena o ciclo onde essa função vai poder ser chamada novamente
+  static uint32_t disabledUntil = 0;
+
+  if(cycle > disabledUntil) {
+    int16_t pedalValue = analogRead(PEDALPIN);
+    disabledUntil = cycle + 10; // Desabilitado por 100ms
+
+    // Decidindo velocidade
+    if(pedalValue > 400 && speed < 100) {
+      speed += (pedalValue - 400) / 160;
+      updateDisplaySpeed();
+    }
+    else if(pedalValue <= 300 && speed > -30) {
+      speed += (pedalValue - 200) / 80;
+      updateDisplaySpeed();
+    }
+  }
+}
+
+// Chamada quando o apenas o paínel está ligado
+void startEngine() {
+  // Esta é a unica funcionalidade ativa enquanto o carro está com apenas o painel ligado
+  updateHeadlight();
+
+  // Tenta ligar o motor do carro
+  if(keyValue >= ENGINE) {
+    // Mostrando os valores
+    lcd.setCursor(15, 0);
+    lcd.print("0 kmh");
+    lcd.setCursor(13, 1);
+    lcd.print("[     ]");
+    lcd.setCursor(11, 3);
+    lcd.print("Desligado");
+
+    carState = 2;
+  }
+
+  // Tenta desligar o sistema
+  turnSystemOff();
+}
+
+// Função responsável por atualizar o campo que mostra o estado atual do farol
+// Chamada apenas quando há alteração nos valores
+void updateDisplayHeadlight() {
+  lcd.setCursor(11, 2);
+
+  // Máquina de estados
+  // Os espaços em branco servem para formatação e para apagar os caracteres não sobrepostos
+  // pelas palavras menores
+  switch(headlightState) {
+    case 0: {
+      lcd.print("Desligado");
+      break;
+    }
+    case 1: {
+      lcd.print("    Baixo");
+      break;
+    }
+    case 2: {
+      lcd.print("     Alto");
+      break;
+    }
+  }
+}
+
+// Função responsável por mudar os estados dos farois
+// Chamada a cada ciclo
+void updateHeadlight() {
+  // Armazena o ciclo onde essa função vai poder ser chamada novamente
+  static uint32_t disabledUntil = 0;
+
+  if(disabledUntil < cycle) {
+    bool headlightButton = !digitalRead(HEADLIGHTBUTTONPIN);
+
+    if(headlightButton) {
+      disabledUntil = cycle + 20; // Desabilitado por 200ms
+
+      // Máquina de estados 
+      switch(headlightState) {
+        case 0: {
+          digitalWrite(SMALLLEDPIN, HIGH);
+          digitalWrite(BIGLEDPIN, LOW);
+          headlightState = 1;
+          break;
+        }
+        case 1: {
+          digitalWrite(SMALLLEDPIN, HIGH);
+          digitalWrite(BIGLEDPIN, HIGH);
+          headlightState = 2;
+          break;
+        }
+        case 2: { 
+          digitalWrite(SMALLLEDPIN, LOW);
+          digitalWrite(BIGLEDPIN, LOW);
+          headlightState = 0;
+          break;
+        }
+      }
+
+      updateDisplayHeadlight();
+    }
+  }
+}
+
+// Função responsável pelo controle do ar condicionado
+// Chamada a cada ciclo
+void updateAC() {
+  // Armazena o ciclo onde essa função vai poder ser chamada novamente
+  static uint32_t disabledUntil = 0;
+
+  if(disabledUntil < cycle) {
+    bool ACButton = !digitalRead(ACBUTTONPIN);
+
+    if(ACButton) {
+      disabledUntil = cycle + 500; // Desabilitado por 5 segundos
+
+      // Inverte e mostra no display a informação correspondente
+      ACon = !ACon;
+      lcd.setCursor(11, 3);
+      ACon ? lcd.print("   Ligado") : lcd.print("Desligado");
+    }
+  }
+}
+
+// Função responsável por atualizar o display com a informação da proximidade
+// Chamada apenas quando há mudança no valor
+void updateDisplayProximity() {
+  // Calcula quantos espacos vazios terão
+  // fmin serve para garantir que nada saia do controle quando a distancia for muito grande
+  // d >= 50:      [     ]
+  // 50 > d >= 40: [#    ]
+  // 40 > d >= 30: [##   ]
+  // 30 > d >= 20: [###  ]
+  // 20 > d >= 10: [#### ]
+  // 10 > d >= 0:  [#####]
+  uint8_t espacos = floorf(fmin(distance, 50) / 10);
+  uint8_t cursorX = 14; // Cursor no primerio espaço para percorrer os outros 5 e ir preenchedo-os conforme o valor de distancia
+
+  // Percorre os espaços preenchendo com #
+  for(;cursorX < 19 - espacos; cursorX++) {
+    lcd.setCursor(cursorX, 1);
+    lcd.print("#");
+  }
+
+  // Percorre os espaços restantes preenchendo com espaços vazios
+  for(;cursorX < 19; cursorX++) {
+    lcd.setCursor(cursorX, 1);
+    lcd.print(" ");
+  }
+}
+
+// Função responsável por calcular a distância
+// Chamada a cada 50 ciclos
+void updateProximity() {
+  // Armazena o ciclo onde essa função vai poder ser chamada novamente
+  static uint32_t disabledUntil = 0;
+
+  if(disabledUntil < cycle) {
+    digitalWrite(TRIGPIN, HIGH); // Enviando ondas sonoros
+    delayMicroseconds(10); // Espera um pouco
+    digitalWrite(TRIGPIN, LOW); // Para
+
+    // Calcula a distancia baseado no tempo em que o echo foi acionado, velocidade do som e divide por dois para compensar ida e volta do som
+    distance = (pulseIn(ECHOPIN, HIGH) * 0.0343) / 2;
+
+    updateDisplayProximity();
+
+    disabledUntil = cycle + 50; // Desabilitado por 500ms
+  }
+}
+
+// Esta função tenta desligar o carro
+// É chamada a cada 100 ciclos
+void turnEngineOff() {
+  // Armazena o ciclo onde essa função vai poder ser chamada novamente
+  static uint32_t disabledUntil = 0;
+
+  if(disabledUntil < cycle) {
+    disabledUntil = cycle + 100; // Desabilitado por 1 segundo
+
+    // Só desliga o motor quando estiver parado e a chave na posição certa
+    if(speed == 0 && keyValue < ENGINE) {
+      // Limpa parcialmente o display
+      lcd.setCursor(12, 0);
+      lcd.print("        ");
+      lcd.setCursor(13, 1);
+      lcd.print("       ");
+      lcd.setCursor(11, 3);
+      lcd.print("         ");
+
+      carState = 1;
+    }
+  }
+}
+
+// Esta função tenta desligar o sistema
+// É chamada a cada 100 ciclos
+void turnSystemOff() {
+  // Armazena o ciclo onde essa função vai poder ser chamada novamente
+  static uint32_t disabledUntil = 0;
+
+  if(disabledUntil < cycle) {
+    disabledUntil = cycle + 100; // Desabilitado por 1 segundo
+
+    // Só desliga se a chave estiver na posição certa
+    if(keyValue < SYSTEM) {
+      // Desliga farois e display
+      digitalWrite(SMALLLEDPIN, LOW);
+      digitalWrite(BIGLEDPIN, LOW);
+      lcd.clear();
+      lcd.noBacklight();
+
+      // Desliga a buzina
+      analogWrite(BUZZERPIN, 0);
+
+      headlightState = 0;
+      carState = 0;
+    }
+  }
+}
+
+// Função responsável por atualizar o carro a cada ciclo
+void run() {
+  updateSpeed();
+  updateHeadlight();
+  updateAC();
+  updateProximity();
+
+  // Tenta desligar o carro
+  turnEngineOff();
+}
+
+void setup() {
+  // Inicializando variáveis
+  cycle = 0;
+  carState = 0;
+  speed = 0;
+  headlightState = 0;
+  ACon = false;
+
+  // Configurando display
+  lcd.init();
+
+  // Configurando pinos digitais
+  pinMode(SMALLLEDPIN, OUTPUT);
+  pinMode(BIGLEDPIN, OUTPUT);
+  pinMode(TRIGPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(HEADLIGHTBUTTONPIN, INPUT_PULLUP);
+  pinMode(ACBUTTONPIN, INPUT_PULLUP);
+
+  // Inicializando pinos digitais
+  digitalWrite(SMALLLEDPIN, LOW);
+  digitalWrite(BIGLEDPIN, LOW);
+  digitalWrite(TRIGPIN, LOW);
+  analogWrite(BUZZERPIN, 0);
+}
+
+void loop() {
+  // Lê o valor da chave
+  keyValue = analogRead(KEYPIN);
+
+  // Máquina de estados geral do carro
+  switch(carState) {
+    case 0: startSystem(); break;
+    case 1: startEngine(); break;
+    case 2: run();
+  }
+
+  // Contagem do ciclo
+  delay(CYCLETIME);
+  cycle++;
+}
--- PAA/projeto_labirinto/davinascimento_202300027801_labirinto.c
+++ PAA/projeto_labirinto/davinascimento_202300027801_labirinto.c
@@ -1,23 +1,193 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <math.h>
 
+#define TAMBUFFER 10000000
+#define TESTE printf("TESTE\n")
 #define PSTRING(ptr) printf("%s\n", ptr)
+#define PINT(var) printf("%d\n", var)
+#define PCHAR(var) printf("%c\n", var)
 
-char bufferEntrada[10000000];
-char bufferSaida[10000000];
-char* bufferPtr;
+typedef struct buffer {
+    char armazenamento[TAMBUFFER];
+    int32_t cursor;
+} buffer_t;
+
+typedef struct vector2 {
+    int8_t x;
+    int8_t y;
+} vector2_t;
+
+typedef struct backtrack {
+    vector2_t posicao;
+    uint8_t nivel;
+} backtrack_t;
+
+typedef struct pilha {
+    backtrack_t* vetor;
+    int16_t topo;
+} pilha_t;
+
+buffer_t bufferEntrada;  
+buffer_t bufferSaida;
+const vector2_t checagem[4] = {
+    {0, -1},
+    {1, 0},
+    {0, 1},
+    {-1, 0},
+};
+
+uint16_t pegarInt() {
+    uint8_t numChar = 0;
+    uint16_t saida = 0;
+
+    while(bufferEntrada.armazenamento[bufferEntrada.cursor] > ' ') {
+        numChar++;
+        bufferEntrada.cursor++;
+    }
+
+    bufferEntrada.cursor -= numChar;
+
+    while(numChar--) {
+        saida += (bufferEntrada.armazenamento[bufferEntrada.cursor++] - 48) * pow(10, numChar);
+    }
+
+    bufferEntrada.cursor += (bufferEntrada.armazenamento[bufferEntrada.cursor + 1] > ' ' ? 1 : 2);
+
+    return saida;
+}
+
+vector2_t pegarLabirinto(int8_t n_Colunas, int8_t n_Linhas, char local[n_Linhas][n_Colunas]) {
+    vector2_t saida;
+    
+    for(int8_t i_Linhas = 0; i_Linhas < n_Linhas; i_Linhas++) {
+        for(int8_t i_Colunas = 0; i_Colunas < n_Colunas; i_Colunas++) {
+            local[i_Linhas][i_Colunas] = bufferEntrada.armazenamento[bufferEntrada.cursor];
+            bufferEntrada.cursor += 2;
+
+            if(local[i_Linhas][i_Colunas] == 'X') {
+                saida.x = i_Colunas;
+                saida.y = i_Linhas;
+            }
+        }
+    }
+
+    return saida;
+}
+
+void escrever(char* string) {
+    for(uint8_t i = 0; string[i] != '\0'; i++) {
+        bufferSaida.armazenamento[bufferSaida.cursor++] = string[i]; 
+    }
+}
+
+void escreverInt(uint8_t numero) {
+    if(numero) {
+        while(numero) {
+            bufferSaida.armazenamento[bufferSaida.cursor++] = numero % 10 + 48;
+            numero /= 10;
+        }
+    }
+    else {
+        bufferSaida.armazenamento[bufferSaida.cursor++] = '0';
+    }
+}
+
+void escreverVector2(vector2_t valor) {
+    escreverInt(valor.y);
+    escrever(","); 
+    escreverInt(valor.x); 
+}
+
+void escreverLn() {
+    bufferSaida.armazenamento[bufferSaida.cursor++] = '\n';
+}
 
 int main(int argc, char** argv) {
-    int32_t n_Labirintos;
-    int32_t n_Colunas;
-    int32_t n_Linhas;
+    bufferEntrada.cursor = 0;
+    bufferSaida.cursor = 0;
 
-    FILE* arqEntrada = fopen(argv[1], "r");
-    fread(bufferEntrada, sizeof(char), 10000000, arqEntrada);
-    fclose(arqEntrada);
+    FILE* arquivo = fopen(argv[1], "r");
+    fread(bufferEntrada.armazenamento, sizeof(char), TAMBUFFER, arquivo);
+    fclose(arquivo);
 
-                
+    for(uint16_t l = 0, n_Labirintos = pegarInt(); l < n_Labirintos; l++) {
+        int8_t n_Colunas = pegarInt();
+        int8_t n_Linhas = pegarInt();
+        char labirinto[n_Linhas][n_Colunas];
+        vector2_t saida = {-1, -1};
+        pilha_t pilha = {
+            (backtrack_t*)malloc(sizeof(backtrack_t) * ((n_Linhas - 2) * (n_Colunas - 2)) + 1), 
+            0
+        };
+        
+        pilha.vetor[0].posicao = pegarLabirinto(n_Colunas, n_Linhas, labirinto);
+        pilha.vetor[0].nivel = 0;
+
+        escrever("L"); escreverInt(l); escrever(":");
+        escrever("INI@"); escreverVector2(pilha.vetor[0].posicao);
+        
+        while(1) {
+            backtrack_t* topo = &pilha.vetor[pilha.topo];
+
+            if(topo->nivel >= 4) {
+                pilha.topo--;
+
+                if(pilha.topo < 0) {
+                    escrever("|FIM@-,-\n");
+                    break;
+                }
+
+                escrever("|BT@"); escreverVector2(topo->posicao); escrever("->");
+                escreverVector2(pilha.vetor[pilha.topo].posicao);
+
+                continue;
+            }
+
+            vector2_t proxPos = {
+                topo->posicao.x + checagem[topo->nivel].x,
+                topo->posicao.y + checagem[topo->nivel].y
+            };
+
+            if(labirinto[proxPos.y][proxPos.x] >= '1') {
+                topo->nivel++;
+                continue;
+            }
+
+            switch(topo->nivel) {
+                case 0: escrever("|F->"); break;
+                case 1: escrever("|D->"); break;
+                case 2: escrever("|T->"); break;
+                case 3: escrever("|E->");
+            }
+
+            topo->nivel++;
+            
+            labirinto[topo->posicao.y][topo->posicao.x] = 'X';
+            topo = &pilha.vetor[++pilha.topo];
+            topo->nivel = 0;
+            topo->posicao = proxPos;
+
+            escreverVector2(topo->posicao);
+            
+            if(topo->posicao.x == 0 || topo->posicao.x == (n_Colunas - 1) || topo->posicao.y == 0 || topo->posicao.y == (n_Linhas - 1)) {
+                saida = topo->posicao;
+
+                escrever("|FIM@");
+                escreverVector2(saida);
+                escreverLn();
+
+                break;
+            }
+        }
+
+        free(pilha.vetor);
+    }
+
+    arquivo = fopen(argv[2], "w");
+    fwrite(bufferSaida.armazenamento, sizeof(char), bufferSaida.cursor, arquivo);
+    fclose(arquivo);
 
     return 0;
 }

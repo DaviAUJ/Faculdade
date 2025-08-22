01844d08fcdcc0fcb85b81911f80442e9f14978f
daviAUJ <daviaraujon@gmail.com>
2025-08-21 23:28:37 -0300

    a

---
+++ Misc/Maratona/CSES/Geometry/LineSegmentIntersection.cpp
@@ -0,0 +1,29 @@
+// https://cses.fi/problemset/task/2190
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST_IO ios_base::sync_with_stdio(0); cin.tie(0)
+#define SLOG(x) cout << x << endl;
+
+struct vec2 {
+    int64_t x;
+    int64_t y ;
+};
+
+int main() {
+    FAST_IO;
+   
+    int32_t cases;
+    vec2 p1, p2, p3, p4;
+    cin >> cases;
+
+    while(cases--) {
+        cin >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y >> p4.x >> p4.y;
+
+        
+    }
+
+    return 0;
+}

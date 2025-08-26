b9e51175ae5dc1cdbf5c15c80a74d175a6377785
daviAUJ <daviaraujon@gmail.com>
2025-08-26 00:06:51 -0300

    a

--- .cfg/nvim/lua/configs/lspconfig.lua
+++ .cfg/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,21 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd" }
-vim.lsp.enable(servers)
+local servers = { "clangd", "rust_analyzer", "postgres_lsp" }
 
+vim.lsp.config.clangd = {
+    cmd = {
+        'clangd',
+        '--clang-tidy',
+        '--background-index',
+        '--offset-encoding=utf-8',
+        '--header-insertion=never',
+        '--function-arg-placeholders=false',
+        '--limit-results=10',
+        '--limit-references=5'
+    },
+    root_markers = { '.clangd', 'compile_commands.json' },
+    filetypes = { 'c', 'cpp', 'h', 'hpp' },
+}
+
+vim.lsp.enable(servers)
 -- read :h vim.lsp.config for changing options of lsp servers 
--- .cfg/nvim/lua/options.lua
+++ .cfg/nvim/lua/options.lua
@@ -1,3 +1,4 @@
 require "nvchad.options"
 
 vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
